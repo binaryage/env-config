f915038c8853df5440dcab206a3f2df67749ec67
Antonin Hildebrand <antonin@hildebrand.cz>
2016-10-16 20:54:20 +0200

    add subsystem for reporting problems

--- src/lib/env_config/core.clj
+++ src/lib/env_config/core.clj
@@ -1,6 +1,7 @@
 (ns env-config.core
   (:require [env-config.impl.read :as read]
-            [env-config.impl.coerce :as coerce]))
+            [env-config.impl.coerce :as coerce]
+            [env-config.impl.report :as report]))
 
 ; -- public api -------------------------------------------------------------------------------------------------------------
 
@@ -12,6 +13,16 @@
 (defn coerce-config [config & [coercers]]
   (coerce/coerce-config config (or coercers default-coercers)))
 
-(defn make-config [prefix vars & [coercers]]
+(defn prepare-config [prefix vars coercers]
   (-> (read-config prefix vars)
       (coerce-config coercers)))
+
+(defn make-config [prefix vars & [coercers reports-atom]]
+  (binding [report/*reports* reports-atom]
+    (prepare-config prefix vars coercers)))
+
+(defn make-config-with-logging [prefix vars & [coercers reporter]]
+  (let [reports-atom (atom [])
+        config (make-config prefix vars coercers reports-atom)]
+    (report/log-reports-if-needed! @reports-atom reporter)
+    config))
