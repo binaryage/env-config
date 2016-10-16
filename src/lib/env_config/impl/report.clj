5181af9f3b875d2c7e5d65b1b40386ada48e7795
Antonin Hildebrand <antonin@hildebrand.cz>
2016-10-16 20:53:07 +0200

    introduce env-config.impl.report

---
+++ src/lib/env_config/impl/report.clj
@@ -0,0 +1,33 @@
+(ns env-config.impl.report
+  (:require [clojure.string :as string]
+            [env-config.impl.logging :as logging]))
+
+(def ^:dynamic *message-prefix* "env-config: ")
+(def ^:dynamic *reports* nil)                                                                                                 ; should be bound if reporting is desired
+
+; -- reporting --------------------------------------------------------------------------------------------------------------
+
+(defn apply-prefix [message]
+  (str *message-prefix* message))
+
+(defn report-warning! [message]
+  (if *reports*
+    (swap! *reports* conj [:warn (apply-prefix message)]))
+  nil)
+
+(defn report-error! [message]
+  (if *reports*
+    (swap! *reports* conj [:error (apply-prefix message)]))
+  nil)
+
+(defn report-info! [message]
+  (if *reports*
+    (swap! *reports* conj [:info (apply-prefix message)]))
+  nil)
+
+; -- standard logging -------------------------------------------------------------------------------------------------------
+
+(defn log-reports-if-needed! [reports & [reporter]]
+  (if (and (or (nil? reporter) (fn? reporter)) (not (empty? reports)))
+    (let [effective-reporter (or reporter logging/reporter)]
+      (effective-reporter reports))))
