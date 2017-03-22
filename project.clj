32c5efdde85254a60257786bf7034fe2a197cc25
Antonin Hildebrand <antonin@hildebrand.cz>
2017-03-22 01:35:49 +0100

    project: move self-host related config under own profile

--- project.clj
+++ project.clj
@@ -20,19 +20,15 @@
   :test-paths ["test/src/tests"]
   :resource-paths ^:replace ["scripts"]
 
-  :cljsbuild {:builds [{:id           "test-build"
-                        :source-paths ["src/lib" "test/src/tests"]
-                        :compiler     {:output-to     "test/resources/.compiled/tests.js"
-                                       :main          'env-config.tests.runner
-                                       :target        :nodejs
-                                       :optimizations :none}}]}
+  :cljsbuild {:builds {}}                                                                                                     ; prevent https://github.com/emezeske/lein-cljsbuild/issues/413
 
   :profiles {:nuke-aliases
              {:aliases ^:replace {}}
 
              :dev
              {:plugins [[com.jakemccrary/lein-test-refresh "0.17.0"]
-                        [lein-tach "0.2.0"]]}
+                        [lein-tach "0.2.0"]
+                        [lein-cljsbuild "1.1.5"]]}
 
              :lib
              ^{:pom-scope :provided}                                                                                          ; ! to overcome default jar/pom behaviour, our :dependencies replacement would be ignored for some reason
@@ -52,20 +48,33 @@
              {:dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]]}
 
              :clojure17
-             {:dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]]}}
+             {:dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]]}
 
-  :aliases {"install"  ["do"
-                        ["shell" "scripts/prepare-jar.sh"]
-                        ["shell" "scripts/local-install.sh"]]
-            "test-all" ["shell" "scripts/test-all.sh"]
-            "jar"      ["shell" "scripts/prepare-jar.sh"]
-            "deploy"   ["shell" "scripts/deploy-clojars.sh"]
-            "release"  ["do"
-                        ["clean"]
-                        ["shell" "scripts/check-versions.sh"]
-                        ["shell" "scripts/prepare-jar.sh"]
-                        ["shell" "scripts/check-release.sh"]
-                        ["shell" "scripts/deploy-clojars.sh"]]}
+             :cljs
+             {:dependencies [[org.clojure/clojurescript "1.9.473" :scope "provided"]]}
 
-  :tach {:debug? false
-         :force-non-zero-exit-on-test-failure? true})
+             :self-host
+             [:cljs
+              {:cljsbuild {:builds [{:id           "self-host-test-build"
+                                     :source-paths ["src/lib"
+                                                    "test/src/tests"]
+                                     :compiler     {:output-to     "test/resources/.compiled/tests.js"
+                                                    :main          'env-config.tests.runner
+                                                    :target        :nodejs
+                                                    :optimizations :none}}]}
+               :tach      {:debug?                               false
+                           :force-non-zero-exit-on-test-failure? true}}]}
+
+  :aliases {"install"        ["do"
+                              ["shell" "scripts/prepare-jar.sh"]
+                              ["shell" "scripts/local-install.sh"]]
+            "test-all"       ["shell" "scripts/test-all.sh"]
+            "test-self-host" ["shell" "scripts/test-self-host.sh"]
+            "jar"            ["shell" "scripts/prepare-jar.sh"]
+            "deploy"         ["shell" "scripts/deploy-clojars.sh"]
+            "release"        ["do"
+                              ["clean"]
+                              ["shell" "scripts/check-versions.sh"]
+                              ["shell" "scripts/prepare-jar.sh"]
+                              ["shell" "scripts/check-release.sh"]
+                              ["shell" "scripts/deploy-clojars.sh"]]})
