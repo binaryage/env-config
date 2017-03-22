b158e8374c8f79beb39e5214a375d840189a9c99
Antonin Hildebrand <antonin@hildebrand.cz>
2017-03-22 01:35:14 +0100

    scripts: introduce a script for testing self-host variants

--- scripts/test-self-host.sh
+++ scripts/test-self-host.sh
@@ -8,21 +8,6 @@ source "./config.sh"
 pushd "$ROOT"
 
 echo ""
-echo "Running tests against Clojure 1.9"
-echo "-----------------------------------------------------------------------------------------------------------------------"
-lein test
-
-echo ""
-echo "Running tests against Clojure 1.8"
-echo "-----------------------------------------------------------------------------------------------------------------------"
-lein with-profile +clojure18 test
-
-echo ""
-echo "Running tests against Clojure 1.7"
-echo "-----------------------------------------------------------------------------------------------------------------------"
-lein with-profile +clojure17 test
-
-echo ""
 echo "Running self-host tests against $(lumo --help | head -n 1 | xargs echo -n)"
 echo "-----------------------------------------------------------------------------------------------------------------------"
 lein with-profile +self-host tach lumo self-host-test-build
